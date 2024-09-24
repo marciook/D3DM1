f8c631934f26a9c470dd5204fb30f9e96002b2e5
marciobtos <marciobtos@gmail.com>
2024-09-23 21:33:39 -0300

    Create ex1.kt

---
+++ aula7/ex1.kt
@@ -0,0 +1,11 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    val media = 7.5
+    if (media < 6)
+    	println("Reprovado")
+    else
+    	println("Aprovado")
+}
