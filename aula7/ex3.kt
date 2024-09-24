60c96bbc677c54ed034d4debbeb2cc9da80ec505
marciobtos <marciobtos@gmail.com>
2024-09-23 21:37:41 -0300

    Create ex3.kt

---
+++ aula7/ex3.kt
@@ -0,0 +1,13 @@
+fun main() {
+    media(4,10)
+
+}
+
+fun media(n1: Int, n2: Int){
+
+    val media = (n1+n2)/2
+    if (media < 6)
+    	println("Reprovado")
+    else
+    	println("Aprovado")
+}
