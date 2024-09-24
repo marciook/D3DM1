3d61bb447dfdf861c5f4828c98ac5ef15cdcb3ab
marciobtos <marciobtos@gmail.com>
2024-09-23 21:58:00 -0300

    Create ex5.kt

---
+++ aula7/ex5.kt
@@ -0,0 +1,33 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    val M1 = Matricula(1,"Marcio",[2.2,3.3])
+
+}
+
+
+class Matricula(var prontuario: Int, var nome: String, var notas: Array<Float>) {
+	
+    /*fun media(n1: Int, n2: Int): String {
+
+    	val media = (n1+n2)/2
+    	if (media < 6)
+    		return "Reprovado"
+    	else
+    		return "Aprovado"
+}
+*/
+}
+/*
+Crie a classe ‘Matrícula’;
+● Ao criar um objeto da classe, devem ser 
+passados como parâmetros: prontuário (Int), 
+nome (String) e notas do aluno (vetor de Float);
+● Deve conter os métodos:
+– getAluno (retorna o nome do aluno em maiúsculo);
+– getMedia (retorna a média final);
+– getResultado (retorna aprovado ou reprovado)
+
+*/
